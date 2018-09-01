d9ee0806ff835df615975c8caa26d76621eddeba
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-08-31 22:02:43 -0300

    First commit api

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var schoolsRouter = require('./routes/schools');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/schools', schoolsRouter);
+
+module.exports = app;
