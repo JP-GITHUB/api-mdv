ef88157bdf5d8b786060772737ec535ba1abd138
Instinct <maxcorreoimo@gmail.com>
2018-09-07 20:06:58 -0300

    mantenedor de usuario funcional

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,10 +2,53 @@ var express = require('express');
 var models = require('../models');
 var router = express.Router();
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
-  let usuarios = await models.USUARIO.findAll(); 
-  res.json({data: usuarios});
+  let usuarios = await models.USUARIO.findAll();
+  res.json({ data: usuarios });
+});
+
+router.put('/', async function (req, res, next) {
+
+  models.USUARIO.update(
+    {
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      rut: req.body.rut,
+      mail: req.body.mail,
+      telefono: req.body.telefono,
+      password: req.body.password
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }
+  )
+    .then(function (rowsUpdated) {
+      res.json(rowsUpdated)
+    })
+    .catch(next)
+
+  res.json({});
+});
+
+router.delete('/', async function (req, res, next) {
+
+  models.USUARIO.update(
+    {
+      estado: 0
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }
+  )
+    .then(function (rowsUpdated) {
+      res.json(rowsUpdated)
+    })
+    .catch(next)
+
+  res.json({});
 });
 
 module.exports = router;
