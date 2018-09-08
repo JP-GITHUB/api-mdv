4c1a90aede21ea42e88f7d0ee3985808cb4ebf3b
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-09-08 06:04:08 -0300

    Deploy

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var path = require('path');
 
 /*importamos archivos de rutas */
 var indexRouter = require('./routes/index');
@@ -19,9 +18,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 /*definimos la ruta inicial y recurso a utilizar*/
 app.use('/', indexRouter); /*se inserta recurso que contiene router de express*/
