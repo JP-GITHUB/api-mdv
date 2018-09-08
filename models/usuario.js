f7ebbda74007d86d29ef483ea2adbc958fb1e454
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-09-08 06:06:15 -0300

    Deploy

--- models/usuario.js
+++ models/usuario.js
@@ -1,83 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     const Usuario = sequelize.define('USUARIO', {
-        nombre: {
-            type: DataTypes.STRING(80),
-            allowNull: false,
-            validate: {
-                notEmpty: {
-                    msg: "Este campo es obligatorio"
-                },
-                len: {
-                    args: [1, 50],
-                    msg: "El campo debe contener un mínimo de 1 y un máximo de 50 caracteres "
-                }
-            }
-
-        },
-        apellido: {
-            type: DataTypes.STRING(80),
-            allowNull: false,
-            validate: {
-                notEmpty: {
-                    msg: "Este campo es obligatorio"
-                },
-                len: {
-                    args: [1, 50],
-                    msg: "El campo debe contener un mínimo de 1 y un máximo de 50 caracteres "
-                }
-            }
-
-        },
+        nombre: DataTypes.STRING(80),
+        apellido: DataTypes.STRING(80),
         rut: DataTypes.CHAR(14),
-        mail: {
-            type: DataTypes.STRING(80),
-            allowNull: false,
-            validate: {
-                isEmail: {
-                    msg: "Debe ingresar un correo válido"
-                }
-            }
-
-        },
-        telefono: {
-            type: DataTypes.STRING(80),
-            allowNull: false,
-            validate: {
-                notEmpty: {
-                    msg: "Este campo es obligatorio"
-                },
-                len: {
-                    args: [8, 8],
-                    msg: "El campo debe contener solo 8 números, no incluya + 56 9"
-                }
-            }
-
-        },
-        password: {
-            type: DataTypes.STRING(8),
-            allowNull: false
-        }
-    }, {
-        instanceMethods: {
-            classMethods: {
-                generateHash: function(password) {
-                    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
-                },
-            },
-
-            validPassword: function(password) {
-                return bcrypt.compareSync(password, this.password)
-            }
-        },
+        mail: DataTypes.STRING(100),
+        telefono: DataTypes.CHAR(100),
+        password: DataTypes.STRING(50),
         estado: DataTypes.BOOLEAN
     }, {
-        tableName: 'USUARIO',
-        underscored: true
-    });
-
-
-    Usuario.associate = function(models) {
+            tableName: 'USUARIO',
+            underscored: true
+        });
+    Usuario.associate = function (models) {
         Usuario.hasMany(models.VENTA);
         Usuario.belongsTo(models.PERFIL, { foreignKey: 'perfil_id' });
     };
