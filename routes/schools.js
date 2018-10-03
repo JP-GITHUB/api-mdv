49df3e6b2355d88d6691c2b0da9c84ace290bfba
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-10-02 23:45:34 -0300

    Agregar colegio

--- routes/schools.js
+++ routes/schools.js
@@ -7,4 +7,40 @@ router.get('/', async function (req, res, next) {
   res.json({ status: true, data: schools });
 });
 
+router.post('/', async function (req, res, next) {
+
+  try {
+    let colegio = await models.COLEGIO.findOne({
+      where: { nombre: req.body.nombre }
+    });
+
+    if (!colegio) {
+      let status_insert = await models.COLEGIO.create({
+        nombre: req.body.nombre,
+        rut: req.body.rut,
+        direccion: req.body.direccion,
+        telefono: req.body.telefono,
+        estado: 1,
+        sucursal_id: req.body.sucursal
+      });
+
+      if (status_insert) {
+        res.json({ status: true, msg: 'Colegio insertado correctamente.' });
+        return;
+      } else {
+        res.json({ status: false, msg: 'Colegio no pudo ser insertado.' });
+        return;
+      }
+    } else {
+      res.json({ status: false, msg: 'Colegio ya existe.' });
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ status: false, msg: 'Colegio no pudo ser insertado.' });
+    return;
+  }
+});
+
+
 module.exports = router;
